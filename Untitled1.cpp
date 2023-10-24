30140c8036fcf89cc6ba3684313689737a90a6ae
TheSnieg <arcadesnowman@gmail.com>
2023-10-24 14:59:07 +0200

    komentarz

---
+++ Untitled1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	short zmienna = 2;
+	cout << "tak";
+	cout << endl;
+	cout << "Przez Przemka";
+	cout << "liczba" << zmienna;
+	return 0;
+}
