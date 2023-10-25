96e79781967e267bb0227a7d7b46f4dbd5f97c67
TheSnieg <arcadesnowman@gmail.com>
2023-10-25 15:02:47 +0200

    komentarz

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -4,10 +4,33 @@ using namespace std;
 
 int main(){
 
+	// deklaracja zmiennych
 	short zmienna = 2;
+	float zmiennoprzecinkowa = 3.21;
+	string tekst = "tekst";
+	char znak = 'a';
+	bool logiczny = false;
+	/*
 	cout << "tak";
 	cout << endl;
 	cout << "Przez Przemka";
-	cout << "liczba" << zmienna;
+	cout << endl; // koniec lini
+	cout << "liczba " << zmienna; //wyswietlenie zmiennych
+	
+	string wyraz;
+	cout << "podaj jakis wyraz ";
+	cin >> wyraz;
+	cout << "podaj wyraz: " << wyraz;
+	*/
+	
+	string imie;
+	cout << "wpisz imie";
+	cin >> imie;
+	
+	float wiek;
+	cout << "podaj wiek";
+	cin >> wiek;
+	cout << "witaj masz na imie " << imie << " i masz " << wiek;
+	
 	return 0;
 }
